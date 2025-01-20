9d9868b8153090c9f3943425423050815a48bcad
herath98 <harshaudayanga401@gmail.com>
2025-01-21 00:31:35 +0530

    changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ import HeroSection from "../components/HomeComponents/HeroSection/HeroSection";
 export default function Page() {
   return (
    <div className=" ">
-     <HeroSection buttonsHidden="hidden" maxWidth="max-w-[818px]" headerText="Tour of Paradise With Golf" normalText="Our mission is to provide golfers with a unique experience through a “Tour of Paradise with Golf”. At PGTC, we turn your dream golf vacations into cherished memories. Combined with the finest accommodations, exquisite cuisine, and curated sightseeing excursions we shall ensure an unforgettable experience for our clients!" imageLocation="/about_home.jpeg"/>
+     <HeroSection buttonsHidden="hidden" maxWidth="max-w-6xl" headerText="Tour of Paradise With Golf" normalText="Our mission is to provide golfers with a unique experience through a “Tour of Paradise with Golf”. At PGTC, we turn your dream golf vacations into cherished memories. Combined with the finest accommodations, exquisite cuisine, and curated sightseeing excursions we shall ensure an unforgettable experience for our clients!" imageLocation="/about_home.jpeg"/>
      <CommitmentSection/>
      <TeamCarousel/>
      <VideoSection/>
