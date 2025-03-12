8fb83d602182d0ba80cc53c2685a19d8caf1c863
harshawebsrus <harsha@websrus.au>
2025-03-12 23:18:47 +0530

    changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
      <CommitmentSection/>
      {/* <TeamCarousel/> */}
      <TeamMembersPage/>
-     <VideoSection/>
+     {/* <VideoSection/> */}
    </div>
   );
 }
